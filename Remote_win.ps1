ca6597e0ff857417da6840f907dcd10f34c9b48b
Bruno Carvalho <34915606+Space-Cowb0y@users.noreply.github.com>
2021-07-28 12:15:18 -0300

    Update Remote_win.ps1

--- Remote_win.ps1
+++ Remote_win.ps1
@@ -7,14 +7,16 @@
 
 ## https://docs.ansible.com/ansible/latest/user_guide/playbooks_environment.html
 ## Executar parte a parte
-## Executar como ADM
+## Executar em um powershell ISE como ADM
+## 
 
+#---------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Upgrade-PowerShell.ps1"
 $file = "$env:temp\Upgrade-PowerShell.ps1"
-$username = ""
-$password = ""
+$username = "Administrador"
+$password = "Rds%200@!"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
@@ -22,7 +24,7 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 # Version can be 3.0, 4.0 or 5.1
 &$file -Version 5.1 -Username $username -Password $password -Verbose
 
-
+#---------------------------
 
 # This isn't needed but is a good security practice to complete
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
@@ -32,7 +34,7 @@ Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
 Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
 
-
+#---------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
@@ -41,6 +43,7 @@ $file = "$env:temp\Install-WMF3Hotfix.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 powershell.exe -ExecutionPolicy ByPass -File $file -Verbose
 
+#---------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
@@ -50,6 +53,7 @@ $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 powershell.exe -ExecutionPolicy ByPass -File $file
 
+#---------------------------
 
 winrm enumerate winrm/config/Listener
 
